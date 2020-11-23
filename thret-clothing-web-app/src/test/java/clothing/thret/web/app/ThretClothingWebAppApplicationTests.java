9f6aa16bc67156b4f95ed4a2391d818de94fae5b
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2020-11-23 15:25:58 +0000

    THRET-14: Generate Spring Workspace

---
+++ thret-clothing-web-app/src/test/java/clothing/thret/web/app/ThretClothingWebAppApplicationTests.java
@@ -0,0 +1,12 @@
+package clothing.thret.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThretClothingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
