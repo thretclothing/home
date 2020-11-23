3e70e94e48c311fbae4de4211f9a2f8b12f0b4c9
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2020-11-23 22:07:46 +0000

    THRET-23: Delegate routing to Angular when not matched by Spring

--- thret-clothing-web-app/src/test/java/clothing/thret/web/app/ThretClothingWebApplicationTests.java
+++ thret-clothing-web-app/src/test/java/clothing/thret/web/app/ThretClothingWebApplicationTests.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ThretClothingWebAppApplicationTests {
+class ThretClothingWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 }
