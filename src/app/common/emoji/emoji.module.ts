8ca6e937dd4773dc7eb421635f6b206501ef8e4f
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2020-11-30 20:43:33 +0000

    THRET-28: Remove twemoji library

--- src/app/common/emoji/emoji.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EmojiDirective } from './directives/emoji.directive';
-
-@NgModule({
-  declarations: [EmojiDirective],
-  exports: [EmojiDirective]
-})
-export class EmojiModule {
-}
