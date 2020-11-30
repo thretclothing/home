5eb45b51d6872e1807dd724439a289fd42649ecc
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2020-11-30 20:43:33 +0000

    THRET-28: Remove twemoji library

--- src/app/common/footer/footer.module.ts
+++ src/app/common/footer/footer.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AssetsModule } from '../assets/assets.module';
-import { EmojiModule } from '../emoji/emoji.module';
 
 @NgModule({
-  imports: [CommonModule, FontAwesomeModule, AssetsModule, EmojiModule],
+  imports: [CommonModule, FontAwesomeModule, AssetsModule],
   declarations: [FooterComponent],
   exports: [FooterComponent]
 })
