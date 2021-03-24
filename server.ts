a29bb8e0d96f45556b2bbc3dc504a2e1e1a26a75
Ross MacPhee <10212476+rossyman@users.noreply.github.com>
2021-03-24 23:57:51 +0000

    THRET-31: Enable logging of restored requestor IP addresses

--- server.ts
+++ server.ts
@@ -1,13 +1,24 @@
 import 'zone.js/dist/zone-node';
-import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
+import * as lb from '@google-cloud/logging-bunyan';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
-export function app(): express.Express {
+export interface Logger {
+  info: (data: unknown) => void;
+}
 
+export interface RequestWithLogger extends express.Request {
+  log: Logger;
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export async function app(): Promise<[express.Express, Logger | undefined]> {
+
+  const isProductionEnv = process.env.NODE_ENV === 'production';
   const server = express();
   const distFolder = join(process.cwd(), 'dist/thret/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
@@ -18,6 +29,14 @@ export function app(): express.Express {
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
+
+  let logger;
+
+  if (isProductionEnv) {
+    const middlewareResponse = await lb.express.middleware({logName: 'express_logs'});
+    logger = middlewareResponse.logger;
+    server.use(middlewareResponse.mw);
+  }
 
   /**
    * Forward all requests for assets to the static dist folder
@@ -30,10 +49,21 @@ export function app(): express.Express {
    * Catch all other requests and route to the Universal engine
    */
   server.get('*', (req, res) => {
+
+    const cloudflareHeader = req.header('CF-Connecting-IP');
+
+    if (isProductionEnv && cloudflareHeader) {
+      (req as RequestWithLogger).log.info({request: {
+        unwrappedIp: cloudflareHeader,
+        resource: req.originalUrl,
+        method: req.method
+      }});
+    }
+
     res.render(indexHtml, {req, providers: [{provide: APP_BASE_HREF, useValue: req.baseUrl}]});
   });
 
-  return server;
+  return [server, logger];
 }
 
 /**
@@ -41,7 +71,7 @@ export function app(): express.Express {
  * '__non_webpack_require__' is a proxy to Node 'require'
  * The below code is to ensure that the server is run only when not requiring the bundle.
  */
-
+// eslint-disable-next-line @typescript-eslint/naming-convention
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
@@ -50,8 +80,15 @@ if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
 
   const port = process.env.PORT || 8080;
 
-  app().listen(port, () => {
-    console.log(`Node Express server listening on port ${port}`)
+  app().then(([server, logger]) => {
+    server.listen(port, () => {
+
+      if (logger) {
+        logger.info(`Node Express server listening on port ${port}`);
+      } else {
+        console.log(`Node Express server listening on port ${port}`);
+      }
+    });
   });
 }
 
